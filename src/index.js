6b16ea18c7b6561d2b13a3452a9b749534656bab
Albert Nurkaev <albert.nurkaev.cs@gmail.com>
2020-10-08 09:29:40 +0200

    feat: implement the towel soft algorithm function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (typeof matrix === "undefined" || matrix.length === 0) {
+        return [];
+    }
+
+    for (let i = 1; i < matrix.length; i = i + 2) {
+        matrix[i] = matrix[i].reverse();
+    }
+
+    return matrix.flat();
+};
